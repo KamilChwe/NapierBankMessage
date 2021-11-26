1f506d9b6814e2fcc4d6d80472b3710c0347c71b
Kamil Chwedura <kchwedura@gmail.com>
2021-11-26 04:29:03 +0000

    Clean up #1

--- InputFileManager.cs
+++ InputFileManager.cs
@@ -30,36 +30,6 @@ namespace NapierBankMessage
                 string messageType = headerMan.DetectType(header);
                 messageMan.StartProcessing(messageType, header, body);
             }
-
-            #region Old Input Attempt
-            /* Old Input Attempt
-
-            // The CSV doesn't have a header so we must specify this
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-            };
-
-            // Use the textword CSV
-            using (var reader = new StreamReader("input.csv"))
-            {
-                // Open the CSV with the configs set
-                using (var csv = new CsvReader(reader, config))
-                {
-                    var records = csv.GetRecords<InputFile>().ToList();
-
-                    for(int i = 0; i < records.Count; i++)
-                    {
-                        header = records[i].Header;
-                        body = records[i].Body;
-
-                        string messageType = headerMan.DetectType(header);
-                        messageMan.StartProcessing(messageType, header, body);
-                    }
-                }
-            }
-            */
-            #endregion
         }
     }
 
